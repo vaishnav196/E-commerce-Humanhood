6ff1028360d70354ad80d4f8a4b541e8d6816212
vaishnav196 <vaish00721@gmail.com>
2023-12-23 11:55:45 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,45 @@
 import React from "react";
 import { IoSearchSharp } from "react-icons/io5";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { GiHamburgerMenu } from "react-icons/gi";
 export default function Navbar() {
   return (
     <div className="Navbar">
       
       <header>
+
+{/* mobile menu */}
+
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          {/* right off canvas code */}
+        <div class="offcanvas offcanvas-start d-md-block  d-lg-none d-sm-block d-xsm-block " data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+  <div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <p>Try scrolling the rest of the page to see this option in action.</p>
+  </div>
+</div>
           <div class="container-lg">
+          <a class="btn d-md-block d-lg-none d-sm-block d-block d-xsm-block   " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions"><GiHamburgerMenu style={{ fontSize: "35", color: "black" }} /></a>
+{/* right off canvas btn */}
+
+          
             <a class="navbar-brand me-auto" href="#">
               <img
                 src="img/hhlogo.avif"
                 alt="Logo here"
-                className="img-fluid ms-auto"
+                className="img-fluid ms-auto d-sm-none d-md-none d-lg-block d-none "
                 style={{ width: "255px" }}
+              />
+            </a>
+            <a class="navbar-brand me-auto" href="#">
+              <img
+                src="img/hhlogo.avif"
+                alt="Logo here"
+                className="img-fluid  d-sm-block d-md-block d-lg-none ms-auto "
+                style={{ width: "155px" }}
               />
             </a>
             {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -86,13 +112,13 @@ export default function Navbar() {
   <div class="offcanvas-header">
    
   </div>
-  <div class="offcanvas-body h-50">
-  <div class="input-group mb-3 container h-50 ">
+  <div class="offcanvas-body h-auto    " >
+  <div class="input-group mb-3 container-fluid p-0 h-50 ">
    
-      <div className="col-6  d-flex mx-auto">
+      <div className="col-md-6  col-12 d-flex mx-auto">
       <div class="input-group mb-3">
   <input type="text" class="form-control" placeholder="Search" aria-label="Serch" aria-describedby="basic-addon2"/>
-    <button class="btn btn-outline-secondary" type="button"><IoSearchSharp style={{ fontSize: "40", color: "black" }} /></button>
+    <a class="btn btn-outline-light border-3 " type="button"><IoSearchSharp style={{ fontSize: "40", color: "black" }} /></a>
   <div class="input-group-append">
   </div>
 </div>
