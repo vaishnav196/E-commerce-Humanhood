f9d694379733961e82f23e52a69a14c42ac80f70
vaishnav196 <vaish00721@gmail.com>
2023-12-23 19:38:35 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import react from 'react'
-import './App.css';
-import Footer from './Components/Footer';
-import About from './Components/About';
-import Talktherapy from './Components/Talktherapy';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import Navbar from './Components/Navbar';
+import react from "react";
+import "./App.css";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Talktherapy from "./Components/Talktherapy";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-
-
-      <Navbar/>
-      <About/>
-<Talktherapy/>
-<Footer/>
-  </div>
-
+      <Navbar />
+      <About />
+      <Talktherapy />
+      <Footer />
+    </div>
   );
 }
 
