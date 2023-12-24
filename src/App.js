e585bfe012d89828a8ec9efce1bab186c9633336
vaishnav196 <vaish00721@gmail.com>
2023-12-24 16:25:41 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@
 // import "~slick-carousel/slick/slick.css"; 
 // import "~slick-carousel/slick/slick-theme.css";
 import "./App.css";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Talktherapy from "./Components/Talktherapy";
-
+// import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+// import About from "./Components/About";
+// import Talktherapy from "./Components/Talktherapy";
+import Footer from "./Components/Footer";
+import JoinCommunity from "./Components/JoinCommunity";
 
-import Home from "./Components/Home";
+
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <About />
+      <JoinCommunity/>
+      {/* <About />
       <Home/>
   
-      <Talktherapy />
+      <Talktherapy /> */}
       <Footer />
     </div>
   );
---
+++ src/Components/JoinCommunity.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function JoinCommunity() {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col">
+                <h1 className='bg-br'>SMALL STEPS BY HUMANHOOD</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
