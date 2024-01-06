21ec5fa0bc040694fb934b3fbb7484c68073bb88
vaishnav196 <vaish00721@gmail.com>
2024-01-06 16:43:49 +0530

    img

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -71,7 +71,7 @@ export default function Home() {
 
 
       <section className="hamper-bg mb-4">
-        <div className="container py-5">
+        <div className="container py-5 px-5">
           <h1 className="text-center text-906 mb-4">
             Festive Special Wellness Hampers
           </h1>
@@ -80,12 +80,12 @@ export default function Home() {
           </p>
 
           <div className="row ">
-            <div className="col-lg-4 mb-4 mb-lg-0">
+            <div className="col-lg-4  col-md-4 mb-4 mb-lg-0">
               <div>
                 <img
-                  src="images/hamper_img1.webp"
+                  src="img/dayhamp.webp"
                   alt=""
-                  className="img-fluid mb-3"
+                  className="img-fluid mb-3 p-2"
                 />
               </div>
               <div>
@@ -100,12 +100,12 @@ export default function Home() {
                 </center>
               </div>
             </div>
-            <div className="col-lg-4 mb-4 mb-lg-0">
+            <div className="col-lg-4 col-md-4 mb-4 mb-lg-0">
               <div>
                 <img
-                  src="images/hamper_img2.webp"
+                  src="img/attracthamp.webp"
                   alt=""
-                  className="img-fluid mb-3"
+                  className="img-fluid mb-3 p-2 "
                 />
               </div>
               <div>
@@ -123,9 +123,9 @@ export default function Home() {
             <div className="col-lg-4">
               <div>
                 <img
-                  src="images/hamper_img3.webp"
+                  src="img/affirmhamp.webp"
                   alt=""
-                  className="img-fluid mb-3"
+                  className="img-fluid mb-3 p-2"
                 />
               </div>
               <div>
@@ -138,6 +138,89 @@ export default function Home() {
                     <i class="fa-solid fa-arrow-right text-906 ps-2"></i>
                   </a>
                 </center>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      <section className="Giftcard-bg mb-4 text-906 fs-sand">
+        <div className="container py-5 ">
+          <div className="row ">
+            <div className="col-lg-6 mb-4 mb-md-0">
+              <img
+                src="img/giftcard.webp"
+                alt=""
+                className="img-fluid"
+              />
+            </div>
+            <div className="col-lg-6 px-lg-5 ">
+              <h1 className="mb-4">Humanhood Gift Card</h1>
+              <p className="fs-4 m-0">Rs. 1500.00</p>
+
+              <p className="fs-14">
+                <a href="" className=" text-906">
+                  Shipping
+                </a>
+                &nbsp; calculated at checkout
+              </p>
+
+              <p className="fs-14">Denominations</p>
+
+              <div className="d-flex gap-2 mb-3">
+                <a href="" className="card-btn-price">
+                  1500.00
+                </a>
+                <a href="" className="card-btn-price1 ">
+                  3000.00
+                </a>
+                <a href="" className="card-btn-price1">
+                  4000.00
+                </a>
+              </div>
+
+              <p className="fs-14">Quantity</p>
+
+              <div className="Counter">
+                <button onclick="increment()">+</button>
+                <h2 id="counting"></h2>
+                <button onclick="decrement()">-</button>
+              </div>
+
+              <div className="d-grid gap-3 mb-5 cart-button">
+                <a href="" className="btn bg-906">
+                  Add to cart
+                </a>
+                <a href="" className="btn bg-906">
+                  Buy it now
+                </a>
+              </div>
+
+              <div className="d-flex justify-content-between share">
+                <a href="" className="text-906">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    className="lucide lucide-arrow-up-from-line pe-2"
+                  >
+                    <path d="m18 9-6-6-6 6" />
+                    <path d="M12 3v14" />
+                    <path d="M5 21h14" />
+                  </svg>
+                  Share
+                </a>
+                <a href="" className="text-906">
+                  View full details
+                  <i className="fa-solid fa-arrow-right ps-2"></i>
+                </a>
               </div>
             </div>
           </div>
